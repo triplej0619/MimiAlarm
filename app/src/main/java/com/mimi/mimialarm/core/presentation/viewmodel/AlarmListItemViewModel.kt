fb7e873373f4e583ffbddcae9fe3a467af618bc2
MihyeLee <mihye.lee@meshkorea.net>
2017-05-24 21:16:46 +0900

    요일 - observable fields 추가

--- app/src/main/java/com/mimi/mimialarm/core/presentation/viewmodel/AlarmListItemViewModel.kt
+++ app/src/main/java/com/mimi/mimialarm/core/presentation/viewmodel/AlarmListItemViewModel.kt
@@ -1,6 +1,8 @@
 package com.mimi.mimialarm.core.presentation.viewmodel
 
 import android.databinding.Bindable
+import android.databinding.Observable
+import android.databinding.ObservableBoolean
 import com.mimi.mimialarm.BR
 
 /**
@@ -8,53 +10,61 @@ import com.mimi.mimialarm.BR
  */
 
 class AlarmListItemViewModel : BaseViewModel() {
-    @get:Bindable
-    var monday: Boolean = true
-        set(value) {
-            field = value
-            notifyPropertyChanged(BR.monday)
-        }
+    var monday: ObservableBoolean = ObservableBoolean(true)
+    var tuesDay: ObservableBoolean = ObservableBoolean(true)
+    var wednesDay: ObservableBoolean = ObservableBoolean(true)
+    var thurseDay: ObservableBoolean = ObservableBoolean(true)
+    var friDay: ObservableBoolean = ObservableBoolean(true)
+    var saturDay: ObservableBoolean = ObservableBoolean(true)
+    var sunDay: ObservableBoolean = ObservableBoolean(true)
 
-    @get:Bindable
-    var tuesDay: Boolean = true
-        set(value) {
-            field = value
-            notifyPropertyChanged(BR.tuesDay)
-        }
-
-    @get:Bindable
-    var wednesDay: Boolean = true
-        set(value) {
-            field = value
-            notifyPropertyChanged(BR.wednesDay)
-        }
-
-    @get:Bindable
-    var thurseDay: Boolean = true
-        set(value) {
-            field = value
-            notifyPropertyChanged(BR.thurseDay)
-        }
-
-    @get:Bindable
-    var friDay: Boolean = true
-        set(value) {
-            field = value
-            notifyPropertyChanged(BR.friDay)
-        }
-
-    @get:Bindable
-    var saturDay: Boolean = true
-        set(value) {
-            field = value
-            notifyPropertyChanged(BR.saturDay)
-        }
-
-    @get:Bindable
-    var sunDay: Boolean = true
-        set(value) {
-            field = value
-            notifyPropertyChanged(BR.sunDay)
-        }
+//    @get:Bindable
+//    var monday: Boolean = true
+//        set(value) {
+//            field = value
+//            notifyPropertyChanged(BR.monday)
+//        }
+//
+//    @get:Bindable
+//    var tuesDay: Boolean = true
+//        set(value) {
+//            field = value
+//            notifyPropertyChanged(BR.tuesDay)
+//        }
+//
+//    @get:Bindable
+//    var wednesDay: Boolean = true
+//        set(value) {
+//            field = value
+//            notifyPropertyChanged(BR.wednesDay)
+//        }
+//
+//    @get:Bindable
+//    var thurseDay: Boolean = true
+//        set(value) {
+//            field = value
+//            notifyPropertyChanged(BR.thurseDay)
+//        }
+//
+//    @get:Bindable
+//    var friDay: Boolean = true
+//        set(value) {
+//            field = value
+//            notifyPropertyChanged(BR.friDay)
+//        }
+//
+//    @get:Bindable
+//    var saturDay: Boolean = true
+//        set(value) {
+//            field = value
+//            notifyPropertyChanged(BR.saturDay)
+//        }
+//
+//    @get:Bindable
+//    var sunDay: Boolean = true
+//        set(value) {
+//            field = value
+//            notifyPropertyChanged(BR.sunDay)
+//        }
 
 }
--- app/src/main/java/com/mimi/mimialarm/core/presentation/viewmodel/AlarmViewModel.kt
+++ app/src/main/java/com/mimi/mimialarm/core/presentation/viewmodel/AlarmViewModel.kt
@@ -1,5 +1,7 @@
 package com.mimi.mimialarm.core.presentation.viewmodel
 
+import android.databinding.ObservableBoolean
+
 /**
  * Created by MihyeLee on 2017. 5. 24..
  */
@@ -12,11 +14,18 @@ class AlarmViewModel : BaseViewModel() {
         alarmList.add(AlarmListItemViewModel())
         alarmList.add(AlarmListItemViewModel())
 
-        alarmList.get(0).friDay = false
-        alarmList.get(0).tuesDay = false
-        alarmList.get(2).monday = false
-        alarmList.get(2).tuesDay = false
-        alarmList.get(2).wednesDay = false
-        alarmList.get(2).thurseDay = false
+        alarmList.get(0).friDay = ObservableBoolean(false)
+        alarmList.get(0).tuesDay = ObservableBoolean(false)
+        alarmList.get(2).monday = ObservableBoolean(false)
+        alarmList.get(2).tuesDay = ObservableBoolean(false)
+        alarmList.get(2).wednesDay = ObservableBoolean(false)
+        alarmList.get(2).thurseDay = ObservableBoolean(false)
+
+//        alarmList.get(0).friDay = false
+//        alarmList.get(0).tuesDay = false
+//        alarmList.get(2).monday = false
+//        alarmList.get(2).tuesDay = false
+//        alarmList.get(2).wednesDay = false
+//        alarmList.get(2).thurseDay = false
     }
 }
