18b0cafeea6d581529cdb8669a5e488eaee648b9
MihyeLee <mihye.lee@meshkorea.net>
2017-06-29 22:11:30 +0900

    알람, 타이머 울리는 화면 액티비티 추가

---
+++ app/src/main/java/com/mimi/mimialarm/android/presentation/view/AlarmOnActivity.kt
@@ -0,0 +1,15 @@
+package com.mimi.mimialarm.android.presentation.view
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.mimi.mimialarm.R
+
+/**
+ * Created by MihyeLee on 2017. 6. 29..
+ */
+class AlarmOnActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_alarm_on)
+    }
+}
